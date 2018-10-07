7722c115782d28cdff26f4c3a27de3096ad60565
themese <jousema.fernandez@gmail.com>
2018-10-07 20:57:10 +0100

    YouTube completed, Redux still missing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
+import _ from "lodash";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
@@ -14,23 +15,27 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
-        YTSearch({ key: API_KEY, term: "berserk" }, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
+        this.videoSearch("Berserk tell me why");
     }
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={(term) => videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={(selectedVideo) => this.setState({ selectedVideo: selectedVideo })}
                     videos={this.state.videos} />
             </div>
         );
+    }
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
     }
 }
 ReactDOM.render(<App />, document.querySelector(".container"));
