2c75d7ad1a9dec23ea88739fd9499d4bc643ed05
themese <jousema.fernandez@gmail.com>
2018-10-07 13:39:39 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyBPz-rAT-kwehyOiktvt_cQgCKLXwfly9Q";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return <div>Hi!!</div>;
+}
+ReactDOM.render(<App />, document.querySelector(".container"));
